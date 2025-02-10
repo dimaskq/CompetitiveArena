7936ef9e86f5c493ce6cbb3538ea99751d2416e2
dimaskq <dimassskq@gmail.com>
2025-02-10 21:45:35 +0200

    secure: true

--- site/rust-frontend/server/index.js
+++ site/rust-frontend/server/index.js
@@ -37,7 +37,7 @@ app.use(
     }),
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 днів
-      secure: false, // У разі локальної розробки, змінити на true для продакшн
+      secure: true,
       httpOnly: true,
       sameSite: 'lax',
     },
