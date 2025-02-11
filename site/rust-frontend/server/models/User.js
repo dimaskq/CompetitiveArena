deece25ec4844ac01b6cfbeaabe58ccf868dec67
dimaskq <dimassskq@gmail.com>
2025-02-11 18:30:59 +0200

    changed structure of User

--- site/rust-frontend/server/models/User.js
+++ site/rust-frontend/server/models/User.js
@@ -4,15 +4,13 @@ const UserSchema = new mongoose.Schema({
   steamId: { type: String, required: true, unique: true },
   displayName: { type: String, required: true },
   avatar: { type: String },
-  info: {
-    wood: { type: Number, default: 0 },
-    stone: { type: Number, default: 0 },
-    methal: { type: Number, default: 0 },
-    sulfur: { type: Number, default: 0 },
-    scrap: { type: Number, default: 0 },
-    mvk: { type: Number, default: 0 },
-    kd: { type: Number, default: 0 },
-  }
+  wood: { type: Number, default: 0 },
+  stone: { type: Number, default: 0 },
+  methal: { type: Number, default: 0 },
+  sulfur: { type: Number, default: 0 },
+  scrap: { type: Number, default: 0 },
+  mvk: { type: Number, default: 0 },
+  kd: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', UserSchema);
