3ab041e70502cfc541f563335ebef1b8a1c47b3c
dimaskq <dimassskq@gmail.com>
2025-02-16 22:22:05 +0200

    added small text for about

--- site/rust-frontend/src/components/pages/contactPage/AboutPage.jsx
+++ site/rust-frontend/src/components/pages/contactPage/AboutPage.jsx
@@ -17,10 +17,10 @@ function AboutPage() {
                                 <h2 className='about__text_offer'>What We Offer:</h2>
                                 <ul className='about__text_list'>
                                     <li className='about__list_item'>Best Game Servers – just enjoy the game!</li>
-                                    <li className='about__list_item'>Exciting Tournaments </li>
-                                    <li className='about__list_item'>Fair Play</li>
-                                    <li className='about__list_item'>Active Community</li>
-                                    <li className='about__list_item'>Player Support </li>
+                                    <li className='about__list_item'>Exciting Tournaments - Exciting tournaments for real champions. </li>
+                                    <li className='about__list_item'>Fair Play - Fair play without cheaters and unfair advantages.</li>
+                                    <li className='about__list_item'>Active Community - A friendly community of players ready to communicate and play together.</li>
+                                    <li className='about__list_item'>Player Support - Fast and responsive support to resolve any issues.</li>
                                 </ul>
                             </div> 
                         </div>
@@ -28,11 +28,11 @@ function AboutPage() {
                             <div className="about__list_container">
                                 <h2 className='about__text_offer'>Why Choose Us?</h2>
                                 <ul className='about__text_list'>
-                                    <li className='about__list_item'>Quality Servers</li>
-                                    <li className='about__list_item'>Regular Tournaments</li>
-                                    <li className='about__list_item'>Security and Fairness</li>
-                                    <li className='about__list_item'>Strong Community</li>
-                                    <li className='about__list_item'>Continuous Development</li>
+                                    <li className='about__list_item'>Quality Servers – Powerful and stable servers for smooth gaming experience.</li>
+                                    <li className='about__list_item'>Regular Tournaments – Regular tournaments with exciting competitions.</li>
+                                    <li className='about__list_item'>Security and Fairness - Fair play and reliable protection from violators.</li>
+                                    <li className='about__list_item'>Strong Community – An active community of like-minded people and players.</li>
+                                    <li className='about__list_item'>Continuous Development – Constant updates and improvements for a better experience.</li>
                                 </ul>
                             </div>
                             <img src={img2} alt="image for second block of about" />
