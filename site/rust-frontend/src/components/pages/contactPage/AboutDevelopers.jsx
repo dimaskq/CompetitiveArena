aa15ef647afd50a5453f0cb65df7a4d2f622aac4
dimaskq <dimassskq@gmail.com>
2025-02-17 21:13:51 +0200

    developer's carts 4.0

--- site/rust-frontend/src/components/pages/contactPage/AboutDevelopers.jsx
+++ site/rust-frontend/src/components/pages/contactPage/AboutDevelopers.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import dmytroIMG from "../../../../public/Dmytro.jpg"
-const developers = [
-  { id: 1, name: "Kravchenko Dmytro", role: "Fullstack Developer", img: dmytroIMG },
-  { id: 2, name: "Serhiy Buhtiyarov", role: "Plugin Developer", img: dmytroIMG },
-  { id: 3, name: "Ilarion Parkhomenko", role: "Social Media Manager", img: dmytroIMG }
-];
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
-const AboutDevelopers = () => {
-  return (
-    <div className="developers">
+const developers = [
+    { id: 1, name: "Kravchenko Dmytro", role: "Fullstack Developer", img: dmytroIMG, delay: 2100 },
+    { id: 2, name: "Serhiy Buhtiyarov", role: "Plugin Developer", img: dmytroIMG, delay: 2300 },
+    { id: 3, name: "Ilarion Parkhomenko", role: "Social Media Manager", img: dmytroIMG, delay: 2500 }
+];
+  
+  const AboutDevelopers = () => {
+    return (
+      <div className="developers">
         <h2 className="developers__title">Our Team</h2>
         <div className="developers__container">
-            {developers.map((dev) => (
-                <div key={dev.id} className="developers__card">
-                <div className="developers__image">
-                    <img src={dev.img} alt={dev.name} />
-                </div>
-                <div className="developers__info">
-                    <h3 className="developers__name">{dev.name}</h3>
-                    <p className="developers__role">{dev.role}</p>
-                </div>
-                </div>
-            ))}
+          {developers.map((dev) => (
+            <div 
+              key={dev.id} 
+              className="developers__card" 
+              data-aos="flip-left" 
+              data-aos-delay={dev.delay}
+            >
+              <div className="developers__image">
+                <img src={dev.img} alt={dev.name} />
+              </div>
+              <div className="developers__info">
+                <h3 className="developers__name">{dev.name}</h3>
+                <p className="developers__role">{dev.role}</p>
+              </div>
+            </div>
+          ))}
         </div>
-    </div>
-  );
-};
-
+      </div>
+    );
+  };
 export default AboutDevelopers;
