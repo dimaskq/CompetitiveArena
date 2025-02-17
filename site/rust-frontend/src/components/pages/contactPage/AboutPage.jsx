051d25c03c1fde78f3eca3401be3543422cdcbb2
dimaskq <dimassskq@gmail.com>
2025-02-17 12:27:06 +0200

    settings timeout for animations 3.0

--- site/rust-frontend/src/components/pages/contactPage/AboutPage.jsx
+++ site/rust-frontend/src/components/pages/contactPage/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import "./aboutPage-styles/aboutPage.css";
 import img1 from "../../../../public/aboutList-img1.png"
 import img2 from "../../../../public/aboutList-img2.jpeg"
@@ -7,18 +7,10 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
 function AboutPage() {
-    const [isTypewriterFinished, setIsTypewriterFinished] = useState(false);
 
     useEffect(() => {
-        if (isTypewriterFinished) {
-            AOS.init({
-                duration: 1500,
-                easing: 'ease-in-out',
-                once: true, 
-            });
-            AOS.refresh(); // Обновляем AOS после изменения состояния
-        }
-    }, [isTypewriterFinished]);
+        AOS.init({ duration: 1500 });
+      }, []);
 
     return (
         <div className="about-page">
@@ -26,60 +18,50 @@ function AboutPage() {
                 About Us
             </h1>
             <div className="about__container">
-                <div className="about__text_container">
-                    <p className="about_text_top">
-                        <Typewriter
-                            options={{
-                                strings: [
-                                    "Welcome to Competitive Arena — your portal to the world of the best Rust servers!"
-                                ],
-                                autoStart: true,
-                                loop: false,
-                                delay: 40, 
-                                deleteSpeed: 10,
-                                pauseFor: 5000,
-                            }}
-                            onInit={(typewriter) => {
-                                typewriter
-                                    .start()
-                                    .callFunction(() => {
-                                        console.log("Typewriter finished!");
-                                        setIsTypewriterFinished(true);
-                                    });
-                            }}
-                        />
-                    </p>
+                    <div className="about__text_container">
+                        <p className="about_text_top">
+                            <Typewriter
+                                options={{
+                                    strings: [
+                                        "Welcome to Competitive Arena — your portal to the world of the best Rust servers!"
+                                    ],
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 40, 
+                                    deleteSpeed: 10,
+                                    pauseFor: 5000,
+                                }}
+                            />
+                        </p>
 
-                    <div className="about__info_box">
-                        <img src={img1} alt="image for first block of about" {...(isTypewriterFinished ? { "data-aos": "fade-right" } : {})}/>
-                        <div className="about__list_container" {...(isTypewriterFinished ? { "data-aos": "fade-left" } : {})}>
-                            <h2 className='about__text_offer'>What We Offer:</h2>
-                            <ul className='about__text_list'>
-                                <li className='about__list_item'>Best Game Servers – just enjoy the game!</li>
-                                <li className='about__list_item'>Exciting Tournaments - Exciting tournaments for real champions.</li>
-                                <li className='about__list_item'>Fair Play - Fair play without cheaters and unfair advantages.</li>
-                                <li className='about__list_item'>Active Community - A friendly community of players ready to communicate and play together.</li>
-                                <li className='about__list_item'>Player Support - Fast and responsive support to resolve any issues.</li>
-                            </ul>
-                        </div> 
-                    </div>
-                    
-                    <div className="about__info_box">
-                        <div className="about__list_container" {...(isTypewriterFinished ? { "data-aos": "fade-right" } : {})}>
-                            <h2 className='about__text_offer'>Why Choose Us?</h2>
-                            <ul className='about__text_list'>
-                                <li className='about__list_item'>Quality Servers – Powerful and stable servers for smooth gaming experience.</li>
-                                <li className='about__list_item'>Regular Tournaments – Regular tournaments with exciting competitions.</li>
-                                <li className='about__list_item'>Security and Fairness - Fair play and reliable protection from violators.</li>
-                                <li className='about__list_item'>Strong Community – An active community of like-minded people and players.</li>
-                                <li className='about__list_item'>Continuous Development – Constant updates and improvements for a better experience.</li>
-                            </ul>
+                        <div className="about__info_box">
+                            <img src={img1} alt="image for first block of about" data-aos="fade-right"/>
+                            <div className="about__list_container" data-aos="fade-left">
+                                <h2 className='about__text_offer'>What We Offer:</h2>
+                                <ul className='about__text_list'>
+                                    <li className='about__list_item'>Best Game Servers – just enjoy the game!</li>
+                                    <li className='about__list_item'>Exciting Tournaments - Exciting tournaments for real champions. </li>
+                                    <li className='about__list_item'>Fair Play - Fair play without cheaters and unfair advantages.</li>
+                                    <li className='about__list_item'>Active Community - A friendly community of players ready to communicate and play together.</li>
+                                    <li className='about__list_item'>Player Support - Fast and responsive support to resolve any issues.</li>
+                                </ul>
+                            </div> 
                         </div>
-                        <img src={img2} alt="image for second block of about" {...(isTypewriterFinished ? { "data-aos": "fade-left" } : {})}/>
+                        <div className="about__info_box" >
+                            <div className="about__list_container" data-aos="fade-right">
+                                <h2 className='about__text_offer'>Why Choose Us?</h2>
+                                <ul className='about__text_list'>
+                                    <li className='about__list_item'>Quality Servers – Powerful and stable servers for smooth gaming experience.</li>
+                                    <li className='about__list_item'>Regular Tournaments – Regular tournaments with exciting competitions.</li>
+                                    <li className='about__list_item'>Security and Fairness - Fair play and reliable protection from violators.</li>
+                                    <li className='about__list_item'>Strong Community – An active community of like-minded people and players.</li>
+                                    <li className='about__list_item'>Continuous Development – Constant updates and improvements for a better experience.</li>
+                                </ul>
+                            </div>
+                            <img src={img2} alt="image for second block of about" data-aos="fade-left"/>
+                        </div>
                     </div>
-                </div>
-
-                <div className="about__join_container">
+                <div class="about__join_container">
                     <a className='about__join_btn' href="https://discord.gg/jf6UAxab">Join Discord</a>
                 </div>
             </div>    
