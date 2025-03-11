961618d9cdc84f74398a4e61d67588c193d18f34
dimaskq <dimassskq@gmail.com>
2025-03-11 16:24:52 +0200

    rules styles 1.3

--- site/rust-frontend/src/components/pages/rules/RulesPage.jsx
+++ site/rust-frontend/src/components/pages/rules/RulesPage.jsx
@@ -8,25 +8,27 @@ function RulesPage() {
         <div className="rules__info">
           <h2 className="rules__win">1.RULES FOR WINNING</h2>
           <div className="rules__kd">
-            <h3 className="rules__kd_title">KD розраховується за формулою:</h3>
+            <h3 className="rules__kd_title">
+              1.1 KD is calculated by the formula:
+            </h3>
             <p className="rules__kd_formula">
               <code>KD = Murders - (Deaths * W)</code>
             </p>
             <ul className="rules__kd_list">
               <li>
-                <span>Murders</span> – загальна кількість убивств
+                <span>Murders</span> – number of murders
               </li>
               <li>
-                <span>Deaths</span> – кількість смертей
+                <span>Deaths</span> – number of deaths
               </li>
               <li>
-                <span>W</span> – коефіцієнт штрафу за смерть (W = 1.5)
+                <span>W</span> – death penalty coefficient (W = 1.5)
               </li>
             </ul>
           </div>
           <div className="rules__rs">
             <h3 className="rules__rs_titile rules__kd_title">
-              Resource Score (Фермерські бали)
+              1.2 Resource Score
             </h3>
             <p className="rules__rs_info">
               Гравці можуть отримувати бали за збір ресурсів. Коефіцієнти за
@@ -34,27 +36,27 @@ function RulesPage() {
             </p>
             <ul className="rules__rs_list rules__kd_list">
               <li>
-                <strong>Wood (wood)</strong> — <code>0.01x</code>
+                Wood (wood) — <code>0.01x</code>
               </li>
               <li>
-                <strong>Stone (stone)</strong> — <code>0.01x</code>
+                Stone (stone) — <code>0.01x</code>
               </li>
               <li>
-                <strong>Metal (metal)</strong> — <code>0.1x</code>
+                Metal (metal) — <code>0.1x</code>
               </li>
               <li>
-                <strong>Sera</strong> — <code>0.3x</code>
+                Sera— <code>0.3x</code>
               </li>
               <li>
-                <strong>Scrap (scrap)</strong> — <code>0.5x</code>
+                Scrap (scrap) — <code>0.5x</code>
               </li>
               <li>
-                <strong>Metal High Quality (mvk)</strong> — <code>1x</code>
+                Metal High Quality (mvk) — <code>1x</code>
               </li>
             </ul>
-            <p>
-              <strong>Формула розрахунку Resource Score:</strong>
-            </p>
+            <h3 className="rules__rs_title rules__kd_title">
+              1.3 Формула розрахунку Resource Score:
+            </h3>
             <p className="rules__rs_formula rules__kd_formula">
               <code>
                 Resource Score = (wood * 0.01) + (stone * 0.01) + (metal * 0.1)
@@ -72,7 +74,13 @@ function RulesPage() {
             <code>TotalScore = KD + (ResourceScore * 0.01)</code>
           </p>
         </div>
-        <div className="rules__explanation">2.WHAT CAN'T BE DONE</div>
+        <div className="rules__explanation">
+          <h2 className="rules__general rules__win">2.WHAT CAN'T BE DONE</h2>
+          <ul className="rules__general_list rules__kd_list">
+            <li>Use of bugs</li>
+            <li>Racism</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
