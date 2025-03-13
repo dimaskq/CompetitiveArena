b4d7e789674216015b06ea967ae4c1baf00b66f9
dimaskq <dimassskq@gmail.com>
2025-03-14 01:28:43 +0200

    changing folder structure 2

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Rust Server Fight - Website Deployment Guide
 
-Welcome to the Rust Server Fight project! This application is designed to create a server for the popular game *Rust*, where players will compete for prizes. Follow the steps below to deploy the application and get your site up and running.
+Welcome to the Rust Server Fight project! This application is designed to create a server for the popular game _Rust_, where players will compete for prizes. Follow the steps below to deploy the application and get your site up and running.
 
 ## 🚀 Application Deployment
 
-# Root Directory: ./site/rust-frontend
+# Root Directory: ./site/monorepo
+
 # Build Command: `npm run deploy`
+
 # Start Command: `npm run start-server`
---
+++ site/monorepo/frontend/App.jsx
@@ -0,0 +1,35 @@
+import HomePage from "./components/pages/home/HomePage.jsx";
+import AboutPage from "./components/pages/contactPage/AboutPage.jsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+import ServersPage from "./components/pages/servers/Servers.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import RulesPage from "./components/pages/rules/RulesPage.jsx";
+import "./base-styles/reset.css";
+import LeaderboardPage from "./components/pages/leaderboardPage/LeaderboardPage.jsx";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<AboutPage />} />
+            <Route path="/servers" element={<ServersPage />} />
+            <Route path="/rules" element={<RulesPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
