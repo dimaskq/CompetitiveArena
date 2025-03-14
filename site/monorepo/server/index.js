d79d9feece25255f759ce928ae784c4f87b27d6a
dimaskq <dimassskq@gmail.com>
2025-03-14 22:41:25 +0200

    refactoring the server structure 2

--- site/monorepo/server/index.js
+++ site/monorepo/server/index.js
@@ -135,3 +135,9 @@ const PORT = 5173;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
--- site/monorepo/server/middleware/auth.js
+++ site/monorepo/server/middleware/auth.js
@@ -9,10 +9,4 @@ function ensureAuthenticated(req, res, next) {
   res.status(401).json({ message: "Unauthorized" });
 }
 
-app.use(express.static(path.join(__dirname, "../dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
-});
-
 module.exports = { ensureAuthenticated };
