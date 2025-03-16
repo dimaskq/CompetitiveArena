162e451c4ff2363758dfa1b2d402be15818a8adb
dimaskq <dimassskq@gmail.com>
2025-03-16 12:34:10 +0200

    refactoring routes

--- site/monorepo/server/server.js
+++ site/monorepo/server/server.js
@@ -2,18 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-// const SteamStrategy = require("passport-steam").Strategy;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-// const mongoose = require("mongoose");
-// const User = require("./models/User");
 const connectDB = require("./config/db");
-const saveUsersRouter = require("./routes/saveUsers");
-const logoutRouter = require("./routes/logout");
-const usersRouter = require("./routes/users");
-const userRouter = require("./routes/user");
+// const saveUsersRouter = require("./routes/saveUsers");
+// const logoutRouter = require("./routes/logout");
+// const usersRouter = require("./routes/users");
+// const userRouter = require("./routes/user");
 const passport = require("./config/passport");
+const routes = require("./routes");
 
 const { DB_URI, SESSION_SECRET, STEAM_API_KEY, STEAM_RETURN_URL, STEAM_REALM } =
   process.env;
@@ -63,47 +61,6 @@ app.use(
   })
 );
 
-// passport.use(
-//   new SteamStrategy(
-//     {
-//       returnURL: STEAM_RETURN_URL,
-//       realm: STEAM_REALM,
-//       apiKey: STEAM_API_KEY,
-//     },
-//     async (identifier, profile, done) => {
-//       try {
-//         let user = await User.findOne({ steamId: profile.id });
-
-//         if (!user) {
-//           user = new User({
-//             steamId: profile.id,
-//             displayName: profile.displayName,
-//             avatar: profile.photos[2]?.value || "",
-//           });
-//           await user.save();
-//         }
-
-//         return done(null, user);
-//       } catch (err) {
-//         return done(err);
-//       }
-//     }
-//   )
-// );
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.steamId);
-// });
-
-// passport.deserializeUser(async (steamId, done) => {
-//   try {
-//     const user = await User.findOne({ steamId });
-//     done(null, user || null);
-//   } catch (err) {
-//     done(err);
-//   }
-// });
-
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -122,10 +79,7 @@ app.get(
   }
 );
 
-app.use("/api/save-users", saveUsersRouter);
-app.use("/logout", logoutRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/user", userRouter);
+app.use(routes);
 
 const PORT = 5173;
 app.listen(PORT, () =>
