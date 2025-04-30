5ab8f2b9219d95394c2c433b0179e5b93e776714
dimaskq <dimassskq@gmail.com>
2025-04-30 20:46:50 +0300

    update to launch the site 2.1.5

--- site/monorepo/frontend/pages/home/AboutBlockHome.jsx
+++ site/monorepo/frontend/pages/home/AboutBlockHome.jsx
@@ -6,7 +6,9 @@ function AboutBlockHome() {
     <section className="aboutBlockHome">
       <div className="slogan__container_about container">
         <div className="slogan__box">
-          <h2 className="slogan__box_title">Learn more about us!</h2>
+          <h2 className="slogan__box_title slogan__box_title2">
+            Learn more about us!
+          </h2>
           <p className="slogan__box_information">
             Curious about who we are and what we do? Click the button below to
             discover our story, mission, and how we work. Find out what drives
