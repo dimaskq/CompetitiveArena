814fe03cd102df2e7065c8c6fd0c48c00fbb17a5
dimaskq <dimassskq@gmail.com>
2025-05-06 13:47:30 +0300

    server page

--- site/frontend/App.jsx
+++ site/frontend/App.jsx
@@ -7,7 +7,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
-// import ServersPage from "./pages/servers/Servers.jsx";
+import ServersPage from "./pages/servers/Servers.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import RulesPage from "./pages/rules/RulesPage.jsx";
 import "./base-styles/reset.css";
@@ -22,7 +22,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/contact" element={<AboutPage />} />
-            {/* <Route path="/servers" element={<ServersPage />} /> */}
+            <Route path="/servers" element={<ServersPage />} />
             <Route path="/rules" element={<RulesPage />} />
             {/* <Route path="/leaderboard" element={<LeaderboardPage />} /> */}
           </Routes>
